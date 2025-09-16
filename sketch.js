f0356e03a9b62a2c9365dfd13c263025665ab317
Atul Kumar <atulkumarshukla458@gmail.com>
2025-09-16 16:17:34 +0530

    added my animal

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
+class Animal {
+  constructor(br = "Human", name = "Nobody", lang = "silence") {
+    this.breed = br;
+    this.name = name;
+    this.language = lang;
+  }
+  talk() {
+    print(this.language);
+  }
+    
+    expressYourself() {
+      print("Hello, I'm " + this.breed + ", I say " + this.language);
+    }
+}
+
 function setup() {
-  // create Black Cat
-  let blackCat = new Animal("Cat", "Blacky", "mewwwww-wwww-ww");
-  
-  //let them talk
-  blackCat.talk();
-  blackCat.expressYourself();
+  let blackCat = new Animal("Dog","lucky", "Bhow Bhow")
+  //blackCat.talk();
+   blackCat.expressYourself();
 }
