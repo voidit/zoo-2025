3af787e9f15088d707ed1681ff2f9e01c4fee470
jutikasawant <jutika.sawant.sod24@anu.edu.in>
2025-09-16 16:21:16 +0530

    I added my Animal

--- Animal.js
+++ Animal.js
@@ -1,19 +1,20 @@
 class Animal {
-  // special function that takes incoming parameters
-  constructor(breed="Human", name="Nobody", language="silence") {
-    //personal properties
-    this.breed = breed;
+  constructor(br = "human", name = "Nobody", lang = "silence") {
+    this.breed = br;
     this.name = name;
-    this.language = language;
+    this.language = lang;
   }
-
-  // personal function
   talk() {
     print(this.language);
   }
+    
+    expressYourself() {
+      print("Hello, I'm " + this.breed + ", I say " + this.language);
+    }
+}
 
-  expressYourself() {
-    // String concotenation example
-    print("Hello, I'm " + this.breed + " , I say " + this.language);
-  }
+function setup() {
+  let snake = new Animal("snake","lucky","pussssss")
+  snake.talk();
+   snake.expressYourself();
 }
