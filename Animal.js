481f7eec230284ef3ca6b7ab03b90cee5f05dcea
radhavelhankar-max <radha.velhankar.sod24@anu.edu.in>
2025-09-16 16:32:45 +0530

    i updated the animal to dog

--- Animal.js
+++ Animal.js
@@ -1,19 +1,27 @@
-class Animal {
-  // special function that takes incoming parameters
-  constructor(breed="Human", name="Nobody", language="silence") {
+class animal {
+//speacial function that takes incoming parameters
+  constructor(br = "Human", name = "Nobody", lang = "silence"){
     //personal properties
-    this.breed = breed;
-    this.name = name;
-    this.language = language;
-  }
-
-  // personal function
+      this.breed= br;
+      this.name = name;
+      this.language =lang;
+}
+//personal function
   talk() {
     print(this.language);
   }
+    expressYourself(){
+     //string concotenation example
+    print("hello,I'm " +this.breed+ " ,I say "+ this.language);
+    }
 
-  expressYourself() {
-    // String concotenation example
-    print("Hello, I'm " + this.breed + " , I say " + this.language);
-  }
+
+}
+
+function setup() {
+ //create pig
+  let dog= new animal("dog","alex","bhaw bhaw")
+  //let pig talk
+   dog.talk();
+   dog.expressYourself();
 }
