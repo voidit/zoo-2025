a3976c9b81caf4dbe57d801c0157716e137bd4f7
krisha0803 <krishas089@gmail.com>
2025-09-17 13:40:17 +0530

    krisha_cat

--- Animal.js
+++ Animal.js
@@ -1,19 +1,27 @@
 class Animal {
-  // special function that takes incoming parameters
-  constructor(breed="Human", name="Nobody", language="silence") {
+  // special function that takes incoming parameters  
+  constructor(br="human", name="Edward", lang="silence"){
     //personal properties
-    this.breed = breed;
-    this.name = name;
-    this.language = language;
+    this.breed= br;
+    this.name= name;
+    this.language = lang;
   }
-
-  // personal function
-  talk() {
+  //personal function 
+  talk(){
     print(this.language);
   }
-
-  expressYourself() {
-    // String concotenation example
-    print("Hello, I'm " + this.breed + " , I say " + this.language);
+    
+    expressYourself(){
+      //String concotenation example 
+      print("Hello, I'm "+ this.breed+". My name is "+ this.name+ ". I say "+ this.language)
   }
+}
+ 
+function setup() {
+  //create Black Cat
+  let blackCat= new Animal("a cat","Edward","mewww-meww-mewww")
+  
+  //let them talk
+  blackCat.talk();
+  blackCat.expressYourself();
 }
