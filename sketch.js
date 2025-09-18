aeb3b6782b60db7643ab4c786c37b46d92cd9756
dishi180 <dishi.soni.sod24@anu.edu.in>
2025-09-18 21:21:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,49 @@
+class Animal {
+  // Constructor to set up animal properties
+  constructor(br = "human", name = "Nobody", lang = "silence") {
+    // Personal properties
+    this.breed = br;
+    this.name = name;
+    this.language = lang;
+  }
+
+  talk() {
+    print(this.language);
+  }
+
+  expressYourself() {
+    // String concatenation example
+    print("Hello, I'm a " + this.breed + " and my name is " + this.name + ". I say " + this.language);
+  }
+}
+
+// Subclass for a Koala
+class Koala extends Animal {
+  // Constructor for the Koala class
+  constructor(name, lang,) {
+    
+    super("Koala", name, lang);
+    
+  }
+ //  Koala class
+  eat() {
+    print("I" + " am munching on some "  + "leaves.");
+  }
+
+  sleep() {
+    print("I"+ " sleep for 15 hours a day.");
+  }
+}
+
 function setup() {
-  // create Black Cat
-  let blackCat = new Cat("Blacky", "straight");
-  let redCat = new Cat("Tiger");
   
-  //let them talk
-  blackCat.talk();
-  blackCat.expressYourself();
-  
-  redCat.talk();
+  let sleepyKoala = new Koala("KOKA", "kkkkk!");
+
+  // Let the koala talk 
+  sleepyKoala.talk();
+ // Let the koala express itself 
+  sleepyKoala.expressYourself();
+
+ sleepyKoala.eat();
+  sleepyKoala.sleep();
 }
